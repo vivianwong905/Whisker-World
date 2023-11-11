b9ba9731008efa03bc5a681c46d2f89934af1bb6
meganm672 <meganm672@gmail.com>
2023-11-11 14:50:45 -0500

    added Super Deluxe Cat products

--- server/db/catProducts.js
+++ server/db/catProducts.js
@@ -251,6 +251,132 @@ const catProducts =[
             "detail":"Tuna treats are extra tastey!",
             "price":10,
             "imageUrl":"https://tikipets.com/wp-content/uploads/2018/09/Tiki-Cat-Crunchy-Treats.png"
+        },  
+        { 
+            "name":"Super Deluxe Beef Dry Food",
+            "detail":"Your cat will love this dry food!",
+            "price":30,
+            "imageUrl":"https://www.boneapatreat.com/cdn/shop/products/Bixbi-Rawbble-Freeze-Dried-Dog-Food_3759b97b-5174-4615-bc79-7823371e9448_1080x.jpg?v=1645562069"
+        },
+      {  
+            "name":"Super Deluxe Beef Wet Food",
+            "detail":"Your cat will love this wet food!",
+            "price":30,
+            "imageUrl":"https://m.media-amazon.com/images/S/aplus-media/vc/9e6265b5-1180-4f76-a173-66dbee9e9485.__CR0,0,300,300_PT0_SX300_V1___.png"
+        },
+        {
+            "name":"Super Deluxe Beef Treat",
+            "detail":"Beef treats are extra tastey!",
+            "price":20,
+            "imageUrl":"https://tikipets.com/wp-content/uploads/2018/09/Tiki-Cat-Crunchy-Treats.png"
+        },
+       { 
+            "name":"Super Deluxe Chicken  Dry Food",
+            "detail":"Your cat will love this dry food!",
+            "price":30,
+            "imageUrl":"https://www.boneapatreat.com/cdn/shop/products/Bixbi-Rawbble-Freeze-Dried-Dog-Food_3759b97b-5174-4615-bc79-7823371e9448_1080x.jpg?v=1645562069"
+        },
+       { 
+            "name":"Super Deluxe Chicken  Wet Food",
+            "detail":"Your cat will love this wet food!",
+            "price":30,
+            "imageUrl":"https://m.media-amazon.com/images/S/aplus-media/vc/9e6265b5-1180-4f76-a173-66dbee9e9485.__CR0,0,300,300_PT0_SX300_V1___.png"
+        },
+        {
+            "name":"Super Deluxe Chicken Treat",
+            "detail":"Chicken treats are extra tastey!",
+            "price":20,
+            "imageUrl":"https://tikipets.com/wp-content/uploads/2018/09/Tiki-Cat-Crunchy-Treats.png"
+        },
+        {
+            "name":"Super Deluxe Duck Dry Food",
+            "detail":"Your cat will love this dry food!",
+            "price":30,
+            "imageUrl":"https://www.boneapatreat.com/cdn/shop/products/Bixbi-Rawbble-Freeze-Dried-Dog-Food_3759b97b-5174-4615-bc79-7823371e9448_1080x.jpg?v=1645562069"
+        },
+        {
+            "name":"Super Deluxe Duck  Wet Food",
+            "detail":"Your cat will love this wet food!",
+            "price":30,
+            "imageUrl":"https://m.media-amazon.com/images/S/aplus-media/vc/9e6265b5-1180-4f76-a173-66dbee9e9485.__CR0,0,300,300_PT0_SX300_V1___.png"
+        },
+        {
+            "name":"Super Deluxe Duck Treat",
+            "detail":"Duck treats are extra tastey!",
+            "price":20,
+            "imageUrl":"https://tikipets.com/wp-content/uploads/2018/09/Tiki-Cat-Crunchy-Treats.png"
+        },
+        {
+            "name":"Super Deluxe Salmon  Dry Food",
+            "detail":"Your cat will love this dry food!",
+            "price":30,
+            "imageUrl":"https://www.boneapatreat.com/cdn/shop/products/Bixbi-Rawbble-Freeze-Dried-Dog-Food_3759b97b-5174-4615-bc79-7823371e9448_1080x.jpg?v=1645562069"
+        },
+        {
+            "name":"Super Deluxe Salmon  Wet Food",
+            "detail":"Your cat will love this wet food!",
+            "price":30,
+            "imageUrl":"https://m.media-amazon.com/images/S/aplus-media/vc/9e6265b5-1180-4f76-a173-66dbee9e9485.__CR0,0,300,300_PT0_SX300_V1___.png"
+        },
+        {
+            "name":"Super Deluxe Salmon Treat",
+            "detail":"Salmon treats are extra tastey!",
+            "price":20,
+            "imageUrl":"https://tikipets.com/wp-content/uploads/2018/09/Tiki-Cat-Crunchy-Treats.png"
+        },
+       { 
+            "name":"Super Deluxe Rabbit  Dry Food",
+            "detail":"Your cat will love this dry food!",
+            "price":30,
+            "imageUrl":"https://www.boneapatreat.com/cdn/shop/products/Bixbi-Rawbble-Freeze-Dried-Dog-Food_3759b97b-5174-4615-bc79-7823371e9448_1080x.jpg?v=1645562069"
+        },
+        {
+            "name":"Super Deluxe Rabbit  Wet Food",
+            "detail":"Your cat will love this wet food!",
+            "price":30,
+            "imageUrl":"https://m.media-amazon.com/images/S/aplus-media/vc/9e6265b5-1180-4f76-a173-66dbee9e9485.__CR0,0,300,300_PT0_SX300_V1___.png"
+        },
+        {
+            "name":"Super Deluxe Rabbit Treat",
+            "detail":"Rabbit reats are extra tastey!",
+            "price":20,
+            "imageUrl":"https://tikipets.com/wp-content/uploads/2018/09/Tiki-Cat-Crunchy-Treats.png"
+        },
+      {  
+            "name":"Super Deluxe Turkey  Dry Food",
+            "detail":"Your cat will love this dry food!",
+            "price":30,
+            "imageUrl":"https://www.boneapatreat.com/cdn/shop/products/Bixbi-Rawbble-Freeze-Dried-Dog-Food_3759b97b-5174-4615-bc79-7823371e9448_1080x.jpg?v=1645562069"
+        },
+        {
+            "name":"Super Deluxe Turkey  Wet Food",
+            "detail":"Your cat will love this wet food!",
+            "price":30,
+            "imageUrl":"https://m.media-amazon.com/images/S/aplus-media/vc/9e6265b5-1180-4f76-a173-66dbee9e9485.__CR0,0,300,300_PT0_SX300_V1___.png"
+        },
+       { 
+            "name":"Super Deluxe Turkey Treat",
+            "detail":"Turkey treats are extra tastey!",
+            "price":20,
+            "imageUrl":"https://tikipets.com/wp-content/uploads/2018/09/Tiki-Cat-Crunchy-Treats.png"
+        },
+        {
+            "name":"Super Deluxe Tuna Dry Food",
+            "detail":"Your cat will love this dry food!",
+            "price":30,
+            "imageUrl":"https://www.boneapatreat.com/cdn/shop/products/Bixbi-Rawbble-Freeze-Dried-Dog-Food_3759b97b-5174-4615-bc79-7823371e9448_1080x.jpg?v=1645562069"
+        },
+        {
+            "name":"Super Deluxe Tuna Wet Food",
+            "detail":"Your cat will love this wet food!",
+            "price":30,
+            "imageUrl":"https://m.media-amazon.com/images/S/aplus-media/vc/9e6265b5-1180-4f76-a173-66dbee9e9485.__CR0,0,300,300_PT0_SX300_V1___.png"
+        },
+       { 
+            "name":"Super Deluxe Tuna Treat",
+            "detail":"Tuna treats are extra tastey!",
+            "price":20,
+            "imageUrl":"https://tikipets.com/wp-content/uploads/2018/09/Tiki-Cat-Crunchy-Treats.png"
         },
         {
             "name":"Cat Top Hat",
@@ -493,6 +619,126 @@ const catProducts =[
             "imageUrl":"https://m.media-amazon.com/images/I/61+jCvtFz2L._AC_SX679_.jpg"
         },
         {
+            "name":"Super Deluxe Cat Top Hat",
+            "detail":"Have your cat look fancy with this hat!",
+            "price":20,
+            "imageUrl":"https://m.media-amazon.com/images/I/61KX+T0h4rL._AC_SY300_SX300_.jpg"
+        },
+        {
+            "name":"Super Deluxe Cat Beanie Hat",
+            "detail":"Your cat will be warm with this hat!",
+            "price":20,
+            "imageUrl":"https://image.chewy.com/is/image/catalog/746782_PT3._AC_SL1200_V1695047300_.jpg"
+        },
+        {
+            "name":"Super Deluxe Cat Vest",
+            "detail":"Dress for winter with this vest!",
+            "price":30,
+            "imageUrl":"https://image.chewy.com/is/image/catalog/743030_MAIN._AC_SL1200_V1692380241_.jpg"
+        },
+        {
+            "name":"Super Deluxe Cat Bow Tie",
+            "detail":"Black tie event ready!",
+            "price":20,
+            "imageUrl":"https://image.chewy.com/is/image/catalog/276288_MAIN._AC_SL1200_V1631658637_.jpg"
+        },
+        {
+            "name":"Super Deluxe Cat Tie",
+            "detail":"Your cat will look dashing with this tie!",
+            "price":20,
+            "imageUrl":"https://m.media-amazon.com/images/I/71AA4uDZ3EL._AC_SX679_.jpg"
+        },
+        {
+            "name":"Super Deluxe Cat Bowler Hat",
+            "detail":"Your cat will love this hat!",
+            "price":30,
+            "imageUrl":"https://i.etsystatic.com/18460845/r/il/7a0387/1839666536/il_1588xN.1839666536_qqrl.jpg"
+        },
+        {
+            "name":"Super Deluxe Cat Ugly Christmas Sweater",
+            "detail":"Get ready to celebrate christmas with your cat",
+            "price":30,
+            "imageUrl":"https://image.chewy.com/is/image/catalog/276568_MAIN._AC_SL1200_V1631663989_.jpg"
+        },
+        {
+            "name":"Super Deluxe Cat Fleece Hoodie",
+            "detail":"Your cat will look so comfy with this sweater",
+            "price":30,
+            "imageUrl":"https://image.chewy.com/is/image/catalog/742550_MAIN._AC_SL1200_V1692380455_.jpg"
+        },
+        {
+            "name":"Super Deluxe Cat Sweater Red",
+            "detail":"Get this sweater so you and your cat can match",
+            "price":30,
+            "imageUrl":"https://m.media-amazon.com/images/I/71olZLTFruL.__AC_SX300_SY300_QL70_FMwebp_.jpg"
+        },
+        {
+            "name":"Super Deluxe Cat Sweater Blue",
+            "detail":"Get this sweater so you and your cat can match",
+            "price":30,
+            "imageUrl":"https://m.media-amazon.com/images/I/71Zd5tv41QL.__AC_SX300_SY300_QL70_FMwebp_.jpg"
+        },
+        {
+            "name":"Super Deluxe Cat Sweater Pink",
+            "detail":"Get this sweater so you and your cat can match",
+            "price":30,
+            "imageUrl":"https://i.etsystatic.com/10664366/r/il/c22fb2/4167997117/il_fullxfull.4167997117_qzyr.jpg"
+        },
+        {
+            "name":"Super Deluxe Cat Insulated Coat",
+            "detail":"Your cat will be ready for winter with this coat",
+            "price":30,
+            "imageUrl":"https://image.chewy.com/is/image/catalog/742262_MAIN._AC_SL1200_V1692379951_.jpg"
+        },
+        {
+            "name":"Super Deluxe Cat Sports Jersey",
+            "detail":"Watch football and wear matching jerseys",
+            "price":30,
+            "imageUrl":"https://littlearth.com/cdn/shop/products/3C20156-BILL-NAME-S_L04_1160x_crop_center.jpg?v=1619216370"
+        },
+        {
+            "name":"Super Deluxe Cat Raincoat",
+            "detail":"Your cat will be singing in the rain with this coat",
+            "price":30,
+            "imageUrl":"https://alwayswhiskered.com/cdn/shop/products/georgie-yellow-raincoat-261058_800x.jpg?v=1629095241"
+        },
+        {
+            "name":"Super Deluxe Cat Boots ",
+            "detail":"Your cats feet will be protected from the cold weather ",
+            "price":30,
+            "imageUrl":"https://s7d2.scene7.com/is/image/PetSmart/5227504?$CLEARjpg$"
+        },
+        {
+            "name":"Super Deluxe Cat Bandana",
+            "detail":"Does your cat like to wear things on their neck then get this bandanna",
+            "price":20,
+            "imageUrl":"https://primalpetfoods.com/cdn/shop/products/PPF-Bandanas-ForSite-Cat_1bc68380-9efa-4ed2-a64c-f1a387270074_grande.jpg?v=1618607328"
+        },
+        {
+            "name":"Super Deluxe Cat Baseball Hat ",
+            "detail":"Your cat can be rocking this baseball hat!",
+            "price":30,
+            "imageUrl":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJN3SEH0blsDXvtg_i4JDyNvXotwgfH7vT1g&usqp=CAU"
+        },
+        {
+            "name":"Super Deluxe Cat Dress",
+            "detail":"Keep your cat elegent with this fancy dress",
+            "price":30,
+            "imageUrl":"https://www.yeswarmg.com/wp-content/uploads/2022/03/Kitty-Costume-for-Cats-Bow-Black-White-Dress-4.webp"
+        },
+        {
+            "name":"Super Deluxe Cat Costume",
+            "detail":"Get ready for halloween with this costume",
+            "price":30,
+            "imageUrl":"https://hips.hearstapps.com/hmg-prod/images/halloween-costumes-for-catsat0-75x-64ad658110c72.png?crop=0.412xw:0.824xh;0.535xw,0.0833xh&resize=640:*"
+        },
+        {
+            "name":"Super Deluxe Cat Birthday Outfit",
+            "detail":"Celebrate your cats birthday in style",
+            "price":30,
+            "imageUrl":"https://m.media-amazon.com/images/I/61+jCvtFz2L._AC_SX679_.jpg"
+        },
+        {
             "name":"Catnip",
             "detail":"Get crazy with this cat nip",
             "price":5,
@@ -731,8 +977,127 @@ const catProducts =[
             "detail":"The bubbles will have kitty asking for more everyday",
             "price":10,
             "imageUrl":"https://image.chewy.com/is/image/catalog/102258_MAIN._AC_SS348_V1589901354_.jpg"
+        },
+        {
+            "name":"Super Deluxe Catnip",
+            "detail":"Get crazy with this cat nip",
+            "price":20,
+            "imageUrl":"https://image.chewy.com/is/image/catalog/153785_MAIN._AC_SS348_V1538754193_.jpg"
+        },
+        {
+            "name":"Super Deluxe Cat Scratcher",
+            "detail":"Scratch up that cardboard",
+            "price":30,
+            "imageUrl":"https://image.chewy.com/is/image/catalog/248057_MAIN._AC_SS348_V1608744153_.jpg"
+        },
+        {
+            "name":"Super Deluxe Cat Tunnel",
+            "detail":"Run and hide in this tunnel and pop out on unsepecting people",
+            "price":30,
+            "imageUrl":"https://image.chewy.com/is/image/catalog/166382_MAIN._AC_SS348_V1668737518_.jpg"
+        },
+        {
+            "name":"Super Deluxe Cat Toy Mouse",
+            "detail":"Play bat the mouse arouund the house",
+            "price":20,
+            "imageUrl":"https://image.chewy.com/is/image/catalog/731710_MAIN._AC_SL1200_V1681330082_.jpg"
+        },
+        {
+            "name":"Super Deluxe Cat Toy Krinkle Ball",
+            "detail":"Kinckle krinkle kitties eyes get larger ",
+            "price":20,
+            "imageUrl":"https://image.chewy.com/is/image/catalog/215394_MAIN._AC_SS348_V1577720874_.jpg"
+        },
+        {
+            "name":"Super Deluxe Cat Treat Ball",
+            "detail":"Smack this ball and you will get a treat",
+            "price":30,
+            "imageUrl":"https://image.chewy.com/is/image/catalog/66556_MAIN._AC_SS348_V1539004998_.jpg"
+        },
+        {
+            "name":"Super Deluxe Cat Wand",
+            "detail":"This wand will keep kitty entertained for hours",
+            "price":30,
+            "imageUrl":"https://image.chewy.com/is/image/catalog/525982_MAIN._AC_SL1200_V1669101576_.jpg"
+        },
+        {
+            "name":"Super Deluxe Cat Spray Catnip",
+            "detail":"Spray the catnip around the house to play with the kitty",
+            "price":20,
+            "imageUrl":"https://image.chewy.com/is/image/catalog/156570_MAIN._AC_SS348_V1634758875_.jpg"
+        },
+        {
+            "name":"Super Deluxe Cat Dental Chew Toy",
+            "detail":"This toy helps to clean kitties teeth while they play",
+            "price":30,
+            "imageUrl":"https://image.chewy.com/is/image/catalog/190721_MAIN._AC_SS348_V1631679740_.jpg"
+        },
+        {
+            "name":"Super Deluxe Cat Feather Toy",
+            "detail":"Feathers are coloful and fun the play with",
+            "price":30,
+            "imageUrl":"https://image.chewy.com/is/image/catalog/204527_MAIN._AC_SS348_V1582825466_.jpg"
+        },
+        {
+            "name":"Super Deluxe Cat Puzzle",
+            "detail":"Treat puzzle that keeps kitty entertained",
+            "price":30,
+            "imageUrl":"https://image.chewy.com/is/image/catalog/227098_MAIN._AC_SS348_V1636510666_.jpg"
+        },
+        {
+            "name":"Super Deluxe Cat Ball Tower Toy",
+            "detail":"Smack the balls around the track ",
+            "price":30,
+            "imageUrl":"https://image.chewy.com/is/image/catalog/161805_MAIN._AC_SS348_V1565736429_.jpg"
+        },
+        {
+            "name":"Super Deluxe Cat Laser Pointer",
+            "detail":"Laser pointer will have your kitty up an jumping in no time",
+            "price":30,
+            "imageUrl":"https://image.chewy.com/is/image/catalog/62876_MAIN._AC_SS348_V1539004637_.jpg"
+        },
+        {
+            "name":"Super Deluxe Cat Sushi Catnip Toy",
+            "detail":"Yum yum sushi catnip toy",
+            "price":30,
+            "imageUrl":"https://image.chewy.com/is/image/catalog/213910_MAIN._AC_SL1200_V1590069698_.jpg"
+        },
+        {
+            "name":"Super Deluxe Catnip Fish Toy",
+            "detail":"Blub blub goes the fishy your cat will love the catnip inside",
+            "price":30,
+            "imageUrl":"https://image.chewy.com/is/image/catalog/278162_MAIN._AC_SL1200_V1615426055_.jpg"
+        },
+        {
+            "name":"Super Deluxe Cat Toy Balls With Bell",
+            "detail":"Kitty loves the balls with bells",
+            "price":20,
+            "imageUrl":"https://image.chewy.com/is/image/catalog/202162_MAIN._AC_SS348_V1569349336_.jpg"
+        },
+        {
+            "name":"Super Deluxe Cat Toy Chirping Bird",
+            "detail":"As kitty interacts with bird it makes a chirping sound",
+            "price":30,
+            "imageUrl":"https://image.chewy.com/is/image/catalog/102753_MAIN._AC_SS348_V1585596820_.jpg"
+        },
+        {
+            "name":"Super Deluxe Cat Narwhal Squeak Toy",
+            "detail":"This cute narwhal will be loved by your kitty",
+            "price":20,
+            "imageUrl":"https://image.chewy.com/is/image/catalog/155045_MAIN._AC_SL1200_V1558466235_.jpg"
+        },
+        {
+            "name":"Super Deluxe Cat Tinsle Balls",
+            "detail":"Smack the balls around the house for kitty enrichment",
+            "price":20,
+            "imageUrl":"https://image.chewy.com/is/image/catalog/748686_MAIN._AC_SS348_V1673630738_.jpg"
+        },
+        {
+            "name":"Super Deluxe Catnip Bubbles",
+            "detail":"The bubbles will have kitty asking for more everyday",
+            "price":20,
+            "imageUrl":"https://image.chewy.com/is/image/catalog/102258_MAIN._AC_SS348_V1589901354_.jpg"
         }
-    
 ]
 
 module.exports= catProducts
