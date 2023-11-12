39aac9dec6dfcb510fc004f4c7681f5a576a3467
meganm672 <meganm672@gmail.com>
2023-11-11 20:20:46 -0500

    set up bones for Redux and created getCatProductsQuery and getSingleCatProductsQuery

--- client/App.jsx
+++ client/App.jsx
@@ -6,9 +6,12 @@ import Products from './components/products';
 import Admin from './components/admin';
 import Checkout from './components/checkout';
 
+import { useSelector } from 'react-redux'
 
 
 function App() {
+  const token = useSelector(state => state.token);
+  console.log("The Token is :", token)
   return (
     <>
       <div id="container">
---
+++ client/redux/api.js
@@ -0,0 +1,84 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const api = createApi({
+    reducerPath: "api",
+
+    baseQuery: fetchBaseQuery({
+        //base url for API calls ... need to change this when deployed
+        baseUrl: "http://localhost:8080/",
+        // set the Content-Type header to the application/json
+        prepareHeaders: (headers, { getState }) => {
+            const token = getState().token
+            headers.set("Content-Type", "application/json")
+            if (token) {
+                headers.set('authorization', `Bearer ${token}`)
+            }
+            return headers
+        },
+    }),
+
+    // define the API endpoints we are trying to access
+    endpoints: (builder) => ({
+        //cat product-
+        //get all the cat products
+        getProducts: builder.query({
+            query: () => "/api/",
+        }),
+        //get single cat product query
+        getSingleProduct: builder.query({
+            query: (productId) => "/api/products/" + productId,
+        }),
+
+        //TODO: create new cat product mutation
+
+        createCatProduct: builder.mutation({}),
+
+        //TODO: update new cat product mutation
+
+        updateCatProduct: builder.mutation({}),
+
+        //TODO: delete cat product mutation
+
+        deleteCatProduct: builder.mutation({}),
+
+        //cart querys and mutations:
+        //TODO: get users items in cart api/users/me/cart query
+
+        getUsersCart: builder.query({}),
+
+        //TODO: create items in users cart api/users/me/cart mutation
+
+        createCartItemsInCart: builder.mutation({}),
+
+        // TODO: update users cart api/users/me/cart/:id mutation
+
+        updateUsersCart: builder.mutation({}),
+
+        // TODO: delete items in users cart api/users/me/cart/:id mutation
+
+        deleteCartItemsInCart: builder.mutation({}),
+
+        //auth folder:
+        //TODO: the /auth/register mutation
+
+        register: builder.mutation({}),
+
+        //TODO: the /auth/login mutation 
+        
+        login: builder.mutation({}),
+
+        //TODO: curently logged in user query /auth/me
+
+        currentUser: builder.query({}),
+
+        //TODO: get all users admin query /auth/
+        getAllUsers: builder.query({}),
+    })
+})
+
+
+export default api;
+
+export const {
+  
+} = api;
