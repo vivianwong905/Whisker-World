2879704dda71f9ce420dafb79539ec058c08dcc4
meganm672 <meganm672@gmail.com>
2023-11-13 09:47:49 -0500

    tried to get single product to work

--- client/App.jsx
+++ client/App.jsx
@@ -6,7 +6,7 @@ import Products from './components/products';
 import Admin from './components/admin';
 import Checkout from './components/checkout';
 import NavBar from './components/Navbar';
-
+import SingleProduct from './components/singleProduct';
 import { useSelector } from 'react-redux'
 
 
@@ -28,6 +28,7 @@ function App() {
             <Route path="/auth/register" element={<Login_register />} />
             <Route path="/cart" element={<Cart />} />
             <Route path="/" element={<Products />} />
+            <Route path="/:id" element={<SingleProduct />}/>
             <Route path="/auth" element={<Admin />} />
             <Route path="/checkout" element={<Checkout />} />
           </Routes>
--- client/components/SingleProduct.jsx
+++ client/components/SingleProduct.jsx
@@ -1,11 +1,63 @@
-const SingleProducts = () => {
+import React from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+
+import { useGetSingleCatProductQuery } from '../redux/api';
+
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+
+
+
+const SingleProduct = () => {
+
+  const params = useParams();
+
+  const catProductId = params.id;
+  const navigate = useNavigate();
+
+  const { data, isLoading, error } = useGetSingleCatProductQuery(catProductId);
+  console.log("cat product", data)
+  if (isLoading) {
+    return <Typography>Loading...</Typography>;
+  }
+
+  if (error) {
+    return <Typography>Error: {error.message}</Typography>;
+  }
+console.error(error)
   return (
-    <>
-      <div className="singleProducts">
-        <h1>this is the SingleProducts component</h1>
-      </div>
-    </>
+    <Box sx={{ margin: 5 }}>
+      {error && !data && (<p>Failed to load Cat Product.</p>)}
+      <Grid container justifyContent="center">
+        <Grid item >
+          <Card sx={{ maxWidth: 370 }}>
+            {/* <CardMedia
+                        component="img"
+                        alt={data.book.title}
+                        height="500"
+                        image={data.book.coverimage}
+                    />
+
+                    <CardContent>
+                        <Typography variant="h3">{data.book.title}</Typography>
+                        <Typography><b>Author:</b> {data.book.author}</Typography>
+                        <Typography><b>Description:</b> {data.book.description}</Typography>
+                        <Typography><b>Available:</b>{data.book.available ? "true" : "false"}</Typography>
+                    </CardContent> */}
+            <CardActions>
+              <Button onClick={() => navigate("/")} > Back </Button>
+            </CardActions>
+          </Card>
+        </Grid>
+      </Grid>
+    </Box>
   );
 };
 
-export default SingleProducts;
+export default SingleProduct;
