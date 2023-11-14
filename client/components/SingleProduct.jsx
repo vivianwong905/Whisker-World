ba0df0642c0b4a6896cbc81003764f2fd290f2c6
meganm672 <meganm672@gmail.com>
2023-11-13 21:32:25 -0500

    single product viewing now just need to resize

--- client/components/SingleProduct.jsx
+++ client/components/SingleProduct.jsx
@@ -19,8 +19,10 @@ const SingleProduct = () => {
   const catProductId = params.id;
   const navigate = useNavigate();
 
-  const { data, isLoading, error } = useGetSingleCatProductQuery(catProductId);
-  console.log("cat product", data)
+  const { data: product, isLoading, error } = useGetSingleCatProductQuery(catProductId);
+
+  console.log("cat product",  product)
+
   if (isLoading) {
     return <Typography>Loading...</Typography>;
   }
@@ -28,25 +30,26 @@ const SingleProduct = () => {
   if (error) {
     return <Typography>Error: {error.message}</Typography>;
   }
+
 console.error(error)
   return (
     <Box sx={{ margin: 5 }}>
       {error && !data && (<p>Failed to load cat product.</p>)}
       <Grid container justifyContent="center">
         <Grid item >
-          <Card sx={{ maxWidth: 370 }}>
-            {/* <CardMedia
+          <Card sx={{ maxWidth: 600 }}>
+            <CardMedia
                         component="img"
-                        alt={data.product.name}
+                        alt={product.name}
                         height="500"
-                        image={data.product.imageUrl}
+                        image={product.imageUrl}
                     />
 
                     <CardContent>
-                        <Typography variant="h3">{data.product.name}</Typography>
-                        <Typography><b>Description:</b> {data.product.detail}</Typography>
-                        <Typography><b>Price:</b>{data.product.price}</Typography>
-                    </CardContent> */}
+                        <Typography variant="h5">{product.name}</Typography>
+                        <Typography><b>Description:</b> {product.detail}</Typography>
+                        <Typography><b>Price:</b>{product.price}</Typography>
+                    </CardContent>
             <CardActions>
               <Button onClick={() => navigate("/")} > Back </Button>
             </CardActions>
