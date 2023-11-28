1f68bf2e09c02cad1515c71a1abdea3a174d6eed
meganm672 <meganm672@gmail.com>
2023-11-28 11:57:29 -0500

    search bar implemented

--- client/components/Products.jsx
+++ client/components/Products.jsx
@@ -1,10 +1,10 @@
 import { useNavigate } from "react-router-dom";
 import { useGetCatProductsQuery, useDeleteCatProductMutation, useCreateCartItemsInCartMutation } from "../redux/api";
-import React from "react";
-import { Button, Box, Card, CardActions, CardContent, CardMedia, Typography, Grid } from "@mui/material";
+import React, { useState } from "react";
+import { Button, Box, Card, CardActions, CardContent, CardMedia, Typography, Grid, TextField } from "@mui/material";
 import NewProductForm from "./NewProductForm";
-
 import { useSelector } from "react-redux";
+
 
 const Products = () => {
   const { user, token } = useSelector(state => state.auth)
@@ -13,7 +13,8 @@ const Products = () => {
   const { data: products, isLoading, error } = useGetCatProductsQuery();
   const [deleteCatProduct] = useDeleteCatProductMutation();
   const [createCartItemsInCart] = useCreateCartItemsInCartMutation();
-  
+
+  const [searchQuery, setSearchQuery] = useState("")
 
   if (isLoading) {
     return <Typography>Loading...</Typography>;
@@ -25,36 +26,51 @@ const Products = () => {
   return (
 
     <Box>
+      <TextField
+        // id="outlined-basic"
+        variant="outlined"
+        label="Search"
+        placeholder="Search Products Here..."
+        onChange={event => setSearchQuery(event.target.value)}
+        sx={{ marginLeft: 10, marginTop: 3, padding:2}}
+      />
       {user?.admin && <NewProductForm />}
       <Typography variant="h3" sx={{ marginLeft: 14 }} >Cat Products</Typography>
       {error && !products && (<p> Failed to load products from api</p>)}
       <Grid container spacing={4} sx={{ marginLeft: 10 }}>
         {products ? (
-          products.map((product) => {
-            return (
-              <Grid item key={product.name} >
-                <Card sx={{ maxWidth: 350, minWidth: 350, maxHeight: 450, minHeight: 450 }} >
-                  <CardMedia
-                    component="img"
-                    alt={product.name}
-                    height="250"
-                    image={product.imageUrl}
-                    sx={{ objectFit: "contain" }}
-                  />
-                  <CardContent>
-                    <Typography variant="h6" sx={{ textAlign: "center" }}>{product.name}</Typography>
-                    <Typography sx={{ textAlign: "center" }}><b>Price:</b>${product.price}</Typography>
-                  </CardContent>
-                  <CardActions sx={{ justifyContent: "center" }}>
-                    <Button variant="contained" onClick={() => navigate("/" + product.id)}>Product Info</Button>
-                    {user?.admin && <Button variant="contained" onClick={() => deleteCatProduct(product.id)}>Delete Product</Button>}
-                    {user?.admin && <Button variant="contained" onClick={() => navigate("/admin",{state:product})}>Update Product</Button>}
-                    {token && <Button variant="contained" onClick={()=> createCartItemsInCart({ quantity: 1, productId: product.id })}>Add to Cart</Button>}
-                  </CardActions>
-                </Card>
-              </Grid>
-            )
-          })) : !error && <p>Loading...</p>}
+          products.filter(product => {
+            if (searchQuery === '') {
+              return product
+            } else if (product.name.toLowerCase().includes(searchQuery.toLowerCase())) {
+              return product;
+            }
+          })
+            .map((product) => {
+              return (
+                <Grid item key={product.name} >
+                  <Card sx={{ maxWidth: 350, minWidth: 350, maxHeight: 450, minHeight: 450 }} >
+                    <CardMedia
+                      component="img"
+                      alt={product.name}
+                      height="250"
+                      image={product.imageUrl}
+                      sx={{ objectFit: "contain" }}
+                    />
+                    <CardContent>
+                      <Typography variant="h6" sx={{ textAlign: "center" }}>{product.name}</Typography>
+                      <Typography sx={{ textAlign: "center" }}><b>Price:</b>${product.price}</Typography>
+                    </CardContent>
+                    <CardActions sx={{ justifyContent: "center" }}>
+                      <Button variant="contained" onClick={() => navigate("/" + product.id)}>Product Info</Button>
+                      {user?.admin && <Button variant="contained" onClick={() => deleteCatProduct(product.id)}>Delete Product</Button>}
+                      {user?.admin && <Button variant="contained" onClick={() => navigate("/admin", { state: product })}>Update Product</Button>}
+                      {token && <Button variant="contained" onClick={() => createCartItemsInCart({ quantity: 1, productId: product.id })}>Add to Cart</Button>}
+                    </CardActions>
+                  </Card>
+                </Grid>
+              )
+            })) : !error && <p>Loading...</p>}
 
       </Grid>
     </Box>
