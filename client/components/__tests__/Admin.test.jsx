5c73c837e979bd52ce03cfbd52755dbe2861313a
meganm672 <meganm672@gmail.com>
2023-11-27 20:32:22 -0500

    no longer having the clearImmediate error and changed the updateProductForm to not be null if not getting the product

--- client/components/__tests__/Admin.test.jsx
+++ client/components/__tests__/Admin.test.jsx
@@ -10,13 +10,29 @@ import {render} from '../../__tests__/utils';
 
 import Admin from '../Admin';
 
+import { usersHandlers } from '../../../mocks/serverMock';
+
+import { setupServer } from 'msw/node'
+
+const server = setupServer(...usersHandlers)
+  
+// Enable API mocking before tests.
+beforeAll(() => server.listen())
+
+// Reset any runtime request handlers we may add during the tests.
+afterEach(() => server.resetHandlers())
+
+// Disable API mocking after the tests are done.
+afterAll(() => server.close())
 
 describe("<Admin />" , () => {
-    
+  
     it("renders the Admin component", ()=>{
         const admin = render(
                     <Admin />
         );
         expect(admin).not.toBe(null);
-    })
+    });
+
+    //TODO: test that it doesn't render if you are not an admin
 })
--- client/components/__tests__/Cart.test.jsx
+++ client/components/__tests__/Cart.test.jsx
@@ -10,6 +10,21 @@ import {render} from '../../__tests__/utils';
 
 import Cart from '../Cart';
 
+import { cartHandlers } from '../../../mocks/serverMock';
+
+import { setupServer } from 'msw/node'
+
+const server = setupServer(...cartHandlers)
+  
+// Enable API mocking before tests.
+beforeAll(() => server.listen())
+
+// Reset any runtime request handlers we may add during the tests.
+afterEach(() => server.resetHandlers())
+
+// Disable API mocking after the tests are done.
+afterAll(() => server.close())
+
 
 describe("<Cart />" , () => {
     
