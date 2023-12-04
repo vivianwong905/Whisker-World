5de7cd9ab55e4b6e64ec6902d2c347d33d54c5f3
Tfia731 <tfia731@gmail.com>
2023-12-04 10:32:57 -0500

    fix client main

--- client/main.jsx
+++ client/main.jsx
@@ -9,6 +9,7 @@ import { Provider } from 'react-redux'
 import store from './redux/store.js'
 
 
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
