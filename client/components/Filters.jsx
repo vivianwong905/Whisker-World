d6e95a3d1961afe55a8c021767a31f56e1cd2e21
meganm672 <meganm672@gmail.com>
2023-12-07 21:04:36 -0500

    restyled the filter component

--- client/components/Filters.jsx
+++ client/components/Filters.jsx
@@ -1,6 +1,6 @@
 
 import React, { useState } from "react";
-import { Box, Card, Checkbox, Slider, Typography, FormGroup, FormControlLabel, FormControl, InputLabel, SwipeableDrawer, Button, } from '@mui/material';
+import { Box, Checkbox, Slider, Typography, FormGroup, FormControlLabel, SwipeableDrawer, Button } from '@mui/material';
 import { useSelector, useDispatch } from "react-redux";
 import { setPriceFilter, setCategoryFilter } from "../redux/filterSlice";
 
@@ -34,38 +34,36 @@ const Filters = () => {
 
   return (
     <div>
-          <Button variant="contained" sx={{ marginTop:1, "&:hover": { bgcolor: "magenta", color: "white" }}} onClick={() => setIsDrawerOpen(true)}>Filter</Button>
-          <SwipeableDrawer
-            anchor="right"
-            open={isDrawerOpen}
-            onClose={() =>setIsDrawerOpen(false)}
-            onOpen={() => setIsDrawerOpen(true)}
-          >
-            <Box sx={{ width:  250, padding: 2 }} role="presentation">
-                <Typography variant="h6" sx={{ paddingBottom: 4, fontSize: 35 }}>Price Filter</Typography>
-                <Slider
-                  getAriaLabel={() => 'Price Range'}
-                  value={Number(price)}
-                  onChange={handlePriceChange}
-                  valueLabelDisplay="on"
-                  valueLabelFormat={(value) => `$${value}`}
-                  min={0}
-                  step={5}
-                  max={50}
-                  marks={marks}
-                  sx={{ paddingTop: 4 }}
-                />
-                <Typography variant="h6" sx={{ paddingTop: 2, fontSize: 35 }}>Category Filter</Typography>
-                <FormGroup >
-                  <FormControlLabel control={<Checkbox onChange={(event) => handleCategoryChange(event, 'Food')} value="Food" />} label={<Typography variant="h5">Food</Typography>} />
-                  <FormControlLabel control={<Checkbox onChange={(event) => handleCategoryChange(event, 'Treat')} value="Treat" />} label={<Typography variant="h5">Treat</Typography>} />
-                  <FormControlLabel control={<Checkbox onChange={(event) => handleCategoryChange(event, 'Toys')} value="Toys" />} label={<Typography variant="h5">Toys</Typography>} />
-                  <FormControlLabel control={<Checkbox onChange={(event) => handleCategoryChange(event, 'Accessories')} value="Accessories" />} label={<Typography variant="h5">Accessories</Typography>} />
-                </FormGroup>
-            </Box>
-          </SwipeableDrawer>
-
-      
+      <Button variant="contained" sx={{ marginTop: 1, "&:hover": { bgcolor: "magenta", color: "white" } }} onClick={() => setIsDrawerOpen(true)}>Filter Products</Button>
+      <SwipeableDrawer
+        anchor="right"
+        open={isDrawerOpen}
+        onClose={() => setIsDrawerOpen(false)}
+        onOpen={() => setIsDrawerOpen(true)}
+      >
+        <Box sx={{ width: 250, padding: 2 }} role="presentation">
+          <Typography variant="h6" sx={{ paddingBottom: 4, fontSize: 35 }}>Price Filter</Typography>
+          <Slider
+            getAriaLabel={() => 'Price Range'}
+            value={Number(price)}
+            onChange={handlePriceChange}
+            valueLabelDisplay="on"
+            valueLabelFormat={(value) => `$${value}`}
+            min={0}
+            step={5}
+            max={50}
+            marks={marks}
+            sx={{ paddingTop: 4 }}
+          />
+          <Typography variant="h6" sx={{ paddingTop: 2, fontSize: 35 }}>Category Filter</Typography>
+          <FormGroup >
+            <FormControlLabel control={<Checkbox onChange={(event) => handleCategoryChange(event, 'Food')} value="Food" />} label={<Typography variant="h5">Food</Typography>} />
+            <FormControlLabel control={<Checkbox onChange={(event) => handleCategoryChange(event, 'Treat')} value="Treat" />} label={<Typography variant="h5">Treat</Typography>} />
+            <FormControlLabel control={<Checkbox onChange={(event) => handleCategoryChange(event, 'Toys')} value="Toys" />} label={<Typography variant="h5">Toys</Typography>} />
+            <FormControlLabel control={<Checkbox onChange={(event) => handleCategoryChange(event, 'Accessories')} value="Accessories" />} label={<Typography variant="h5">Accessories</Typography>} />
+          </FormGroup>
+        </Box>
+      </SwipeableDrawer>
     </div>
   );
 }
