677698604c560359ac8467609f134a99ba761b17
vivian wong <32468366+vivianwong905@users.noreply.github.com>
2024-02-05 08:58:15 -0600

    trying to convert to typescript

--- client/redux/authSlice.ts
+++ client/redux/authSlice.ts
@@ -1,11 +1,19 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { RootState } from './store';
 
 import api from './api';
 
+interface AuthState {
+    token : string | null;
+    user : User | null;
+}
+
+const initialState: AuthState = {token: localStorage.getItem("token"), 
+    user: JSON.parse(localStorage.getItem("user") || "")}
+
 const authSlice = createSlice({
     name: "auth",
-    initialState: {token: localStorage.getItem("token"), 
-    user: JSON.parse(localStorage.getItem("user"))},
+    initialState,
     reducers: {
         logout: successfulLogout
     },
@@ -23,13 +31,35 @@ const authSlice = createSlice({
     }
 });
 //local storage
-function successfulAuth(state, {payload}) {
+type Product = {
+    id: number;
+    name: string;
+    detail: string;
+    price: number;
+    imageUrl: string;
+}
+type CartItem = {
+    quantity: number;
+    product: Product;
+}
+type User = {
+    id: string;
+    username: string;
+    name: string;
+    password: string;
+    cartItems: CartItem[];
+}
+type Payload = {
+    token: string;
+    user: User;
+}
+function successfulAuth(state: RootState, {payload}:{payload: Payload}) {
     localStorage.setItem("token", payload.token)
     localStorage.setItem("user", JSON.stringify(payload.user))
-    return ({token: payload.token, user:payload.user})
+    return payload
 }
 
-function successfulLogout(_state) {
+function successfulLogout() {
     localStorage.removeItem("token")
     localStorage.removeItem("user")
     return ({token: null, user: null})
